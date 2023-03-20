1a24a5adeb5c3931decaa7e5c3f4989d42e1baf1
Sibanengi <mpsibanengi@gmail.com>
2023-03-20 09:24:19 -0700

    dog fix

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -2,28 +2,21 @@
 #define DOG_H
 
 /**
- * struct dog - dog structure
- *
- * @name: name of dog
- * @age: age of the dog
- * @owner: dog owner
- *
-*/
+ * struct dog - Data type of a dog.
+ * @name: Name of Dog.
+ * @age: Age of Dog.
+ * @owner: Owner of Dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char *owner;
+	char  *owner;
 };
-
-/**
- * dog_t -Typedef for struct dog
-*/
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
+typedef struct dog dog_t;
 
 #endif
