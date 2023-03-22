39661f5ff04dc83195c38f6da09a0969c54ec904
Sibanengi <mpsibanengi@gmail.com>
2023-03-22 11:21:44 -0700

    my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include "function_pointers.h"
+
 /**
-* print_name - prints a name
-* @name: name of the person
-* @f: funtion that prints
-* Return: Nothing.
-*/
+ * print_name - prints a name
+ * @name: name of a person.
+ * @f: print name as is and UPPERCASE.
+ * Return: Always 0.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (f)
-f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
