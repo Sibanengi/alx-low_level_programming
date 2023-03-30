e0907f344daf4a0adb7b829f713215572c076bb0
Sibanengi <mpsibanengi@gmail.com>
2023-03-30 14:10:15 -0700

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-/**
- * print_list - prints linked lists
- * @h: list head
- * Return: length
- */
-size_t print_list(const list_t *h)
-{
-	size_t length = 0;
 
-	if (h)
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
 	{
-		while (h)
-		{
-			if (h->str == NULL)
-				printf("[0] %p\n", h->str);
-			else
-				printf("[%d] %s\n", h->len, h->str);
-			length++;
-			h = h->next;
-		}
+		count++;
+		h = h->next;
 	}
-	return (length);
+	return (count);
 }
