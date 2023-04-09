9a91574852378267294bd0ab8cf8168a59fc5f23
Sibanengi <mpsibanengi@gmail.com>
2023-04-09 13:27:57 -0700

    read text file fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,3 +1,7 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
@@ -14,44 +18,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_written;
-	char *buffer;
+int fd, bytes_read, bytes_written;
+char *buffer;
 
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
 
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+free(buffer);
+return (0);
+}
 
-	n_read = read(fd, buffer, letters);
-	if (n_read == -1)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
 
-	buffer[n_read] = '\0';
-
-	n_written = write(STDOUT_FILENO, buffer, n_read);
-	if (n_written != n_read)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-
-	close(fd);
-	free(buffer);
-
-	return (n_written);
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
+close(fd);
+return (bytes_read);
 }
