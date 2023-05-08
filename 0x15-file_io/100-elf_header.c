021c3f16b3aed725e2fa81d0fe9651db4e36c25f
Sibanengi <mpsibanengi@gmail.com>
2023-05-08 15:04:28 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,8 +20,8 @@ void display_elf_header(Elf64_Ehdr *elf_header)
 	printf("Class:                             %s\n",
 		elf_header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
 	printf("Data:                              %s\n",
-	elf_header->e_ident[EI_DATA] == ELFDATA2LSB ?
-	"2's complement, little endian" : "2's complement, big endian");
+		elf_header->e_ident[EI_DATA] == ELFDATA2LSB ?
+		"2's complement, little endian" : "2's complement, big endian");
 	printf("Version:                           %d %s\n",
 		elf_header->e_ident[EI_VERSION], "(current)");
 	printf("OS/ABI:                            %s\n",
@@ -45,6 +45,7 @@ int main(int argc, char **argv)
 {
 	int fd;
 	Elf64_Ehdr elf_header;
+	ssize_t read_result;
 
 	if (argc != 2)
 	{
@@ -59,7 +60,8 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
+	read_result = read(fd, &elf_header, sizeof(elf_header));
+	if (read_result != sizeof(elf_header))
 	{
 		perror("read");
 		close(fd);
