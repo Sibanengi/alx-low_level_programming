0d63595b02570c64b73db3f58e4046d611386d9e
Sibanengi <mpsibanengi@gmail.com>
2023-05-08 12:57:27 -0700

    created header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
