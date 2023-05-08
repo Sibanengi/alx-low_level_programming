93b4a230b06ed61e47472d145f54a22e37ee232e
Sibanengi <mpsibanengi@gmail.com>
2023-05-08 13:27:51 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -12,37 +14,37 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t nread, nwritten;
-char *buf;
+    int fd;
+    ssize_t nread, nwritten;
+    char *buf;
 
-if (filename == NULL)
-return (0);
+    if (filename == NULL)
+        return (0);
 
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-buf = malloc(sizeof(char) * letters);
-if (buf == NULL)
-return (0);
+    buf = malloc(sizeof(char) * letters);
+    if (buf == NULL)
+        return (0);
 
-nread = read(fd, buf, letters);
-if (nread == -1)
-{
-free(buf);
-close(fd);
-return (0);
-}
-nwritten = write(STDOUT_FILENO, buf, nread);
-if (nwritten == -1 || nwritten != nread)
-{
-free(buf);
-close(fd);
-return (0);
-}
+    nread = read(fd, buf, letters);
+    if (nread == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
+    nwritten = write(STDOUT_FILENO, buf, nread);
+    if (nwritten == -1 || nwritten != nread)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
 
-free(buf);
-close(fd);
-return (nwritten);
+    free(buf);
+    close(fd);
+    return (nwritten);
 }
